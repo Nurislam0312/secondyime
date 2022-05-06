fbfe39b2d042397bb892a119ee47da58840cfcc6
Nurislam0312 <nurikshurik2021@mail.ru>
2022-05-06 22:14:31 +0600

    gfhhgfngfjfngfhmjjj

--- src/Interman.java
+++ src/Interman.java
@@ -2,5 +2,10 @@ public class Interman {
     public static void main(String[] args) {
         System.out.println("*");
         System.out.println("BRANCHCLUB");
+
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
     }
 }
